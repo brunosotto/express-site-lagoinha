98ac6d0ac4670f88c87ade50ac98c9a9ef94caed
André Rds <andrerds.souza@gmail.com>
2020-08-18 16:41:12 -0300

    fix no includeJs, fix lista videos, menu mobile

--- src/public/js/instagramConfigLegacy.js
+++ src/public/js/instagramConfigLegacy.js
@@ -2,15 +2,15 @@
 
      $(window).on('load', function() {
           $.instagramFeed({
-                'username': 'lagoinhapromissao',
-                'container': "#instagramLP",
+                'username': 'legacypromissao',
+                'container': "#instagramLegacy",
                 'display_profile': true,
                 'display_biography': true,
                 'display_gallery': true,
                 'callback': null,
                 'styling': true,
-                'items': 8,
-                'items_per_row': 4,
+                'items': 9,
+                'items_per_row': 3,
                 'margin': 1,
                 'lazy_load': true 
      });
